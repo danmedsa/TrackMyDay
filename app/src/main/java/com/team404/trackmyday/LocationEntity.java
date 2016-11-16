71e3e2ec93300e273c9b88b9f0ffa682cb535a1e
danmedsa <daniel.medina-sada@ttu.edu>
2016-11-15 22:13:57 -0600

    Location Improvement

--- app/src/main/java/com/team404/trackmyday/LocationEntity.java
+++ app/src/main/java/com/team404/trackmyday/LocationEntity.java
@@ -1,6 +1,8 @@
 package com.team404.trackmyday;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Created by DanielMedina on 11/15/16.
@@ -10,10 +12,26 @@ public class LocationEntity {
     public double latitude, longitude;
     public String name;
     public Date date;
+
+    //Count diff activities at same location to get most probable activity
+    public ArrayList<String> Activities = new ArrayList<String>();
+    private HashMap<String, Integer> guessing = new HashMap<String, Integer>();
+
     LocationEntity(double lat, double lon, String name, Date date){
         this.latitude = lat;
         this.longitude = lon;
         this.name = name;
         this.date = date;
+    }
+
+    //Count diff activities at same location to get most probable activity
+    public void addActivity(String activity){
+        if(!guessing.containsKey(activity)){
+            guessing.put(activity,1);
+        }else{
+            Integer temp = guessing.get(activity);
+            temp += 1;
+            guessing.put(activity,temp);
+        }
     }
 }
